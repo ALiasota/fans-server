9b3bf50dba443d3ad13a9981ab981b569ad35869
Andrii <liasotaandrii0@gmail.com>
2024-08-17 17:03:02 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 5001;
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.setGlobalPrefix('api');
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
